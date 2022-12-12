7b6df942e7f959df43007eb216662e0843417f83
Mansi Singh <82221645+MansiSingh1117@users.noreply.github.com>
2022-12-12 22:17:35 +0530

    Update app.py

--- Annotation-main/app.py
+++ Annotation-main/app.py
@@ -68,7 +68,7 @@ def cartoonization():
     
 
     img = uploadImage("annotation_img")
-    st.text("Input image")
+    st.title("Input image")
     st.image(img)
     #Bilateral Blurring
     img1b=cv2.bilateralFilter(img,3,75,75)
@@ -112,7 +112,7 @@ def cartoonization():
     #plt.imshow(final,cmap='gray')
     #plt.axis("off")
     plt.savefig('output1', bbox_inches='tight')
-    st.text("cartoonized image")
+    st.title("cartoonized image")
     st.image(final_img)
 
     plt.show()
